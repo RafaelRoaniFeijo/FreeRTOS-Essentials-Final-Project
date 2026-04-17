c638f30d3a843eb82b0769549c07805cf3c9cc6f
rafael.feijo <rafael.feijo@NOTE067.fullgauge.com.br>
2026-04-17 16:26:08 -0300

    :minidisc: Add register_add_card

--- application/register.c
+++ application/register.c
@@ -51,9 +51,75 @@ uint32_t register_cards_registered(register_t *Register)
 	return qtd;			/*Retorna quantos cartoes tem cadastrado*/
 }
 
-register_err_e register_check_card(register_t *Register, uint64_t CardID, uint32_t *IDX);
+register_err_e register_check_card(register_t *Register, uint64_t CardID, uint32_t *IDX)
+{
+	uint32_t i, address;
+	storage_t *Storage = &Register->Storage;
+	reg_card_t *RegCard = &Register->RegCard;
 
-register_err_e register_add_card(register_t *Register, uint64_t CardID);
+	BoardAssert(Register);
+
+	address = REGISTER_EE_CARDS_START;
+
+	for (i = 0; i < REGISTER_MAXIMUM_CARDS; ++i)   												/*Itera por toda a memória ate encontrar o cartão*/
+	{
+		storage_read(Storage, address, (uint8_t*)RegCard, sizeof(reg_card_t));
+
+		if (RegCard->u8UsedMask == REGISTER_USED_MASK && RegCard->u64CardID == CardID)  		/*Se o cartão existe e se está registrado*/
+		{
+			if (IDX != NULL)
+			{
+				*IDX = i;																		/*Retornar a posicao*/
+			}
+			return REGISTER_OK;
+		}
+		address += REGISTER_EE_CARDS_STEP;
+	}
+	return REGISTER_NOT_FOUND;
+}
+
+register_err_e register_add_card(register_t *Register, uint64_t CardID)
+{
+	uint32_t i, freeIDX, address;
+	storage_t *Storage 	= &Register->Storage;
+	reg_card_t *RegCard = &Register->RegCard;
+
+	BoardAssert(Register);
+
+	if (Register->u32FreeSpace == 0)			/*Se não tiver espaço livre*/
+	{
+		return REGISTER_NO_FREE_SPACES;
+	}
+
+	//START WITH AN iNVALID iNDEX
+	freeIDX = 0xFF;
+	address = REGISTER_EE_CARDS_START;
+
+	for (i = 0; i < REGISTER_MAXIMUM_CARDS; ++i)
+	{
+		storage_read(Storage, address, (uint8_t*)RegCard, sizeof(reg_card_t));
+
+		if (RegCard->u8UsedMask == REGISTER_USED_MASK && RegCard->u64CardID == CardID) 		/*Se encontrar uma posição que está usando e há aquele ID*/
+		{
+			return REGISTER_CARD_ALREADY_EXISTS;
+		}
+
+		if (RegCard->u8UsedMask == REGISTER_UNUSED_MASK && freeIDX == 0xFF)					/*Endereco não disponivel e endereco nulo*/
+		{
+			freeIDX = i;
+		}
+		address += REGISTER_EE_CARDS_STEP;
+	}
+	RegCard->u8UsedMask = REGISTER_USED_MASK;
+	RegCard->u64CardID = CardID;
+	address = REGISTER_EE_CARDS_START + (freeIDX * REGISTER_EE_CARDS_STEP);					/*Calculo de qual endereço ira salvar*/
+	storage_write(Storage, address, (uint8_t*)RegCard, sizeof(reg_card_t));
+
+	Register->u32CardsRegistered++;
+	Register->u32FreeSpace--;
+
+	return REGISTER_OK;
+}
 
 uint32_t register_list_cards(register_t *Register, uint64_t *OutArray);
 
